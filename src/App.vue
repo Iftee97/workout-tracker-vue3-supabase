32bbfe27232131d7b33fabf98ceb90408cd30836
Iftee97 <tawsifmasrur@gmail.com>
2022-10-12 15:31:14 +0600

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="min-h-full font-Poppins box-border">
+    <Navigation />
+    <router-view />
+  </div>
+</template>
+
+<script>
+import Navigation from "./components/Navigation.vue";
+
+export default {
+  name: "App",
+
+  components: {
+    Navigation,
+  },
+
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700&display=swap");
+</style>
---
+++ src/views/Create.vue
@@ -0,0 +1,13 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: "create",
+
+  setup() {
+    return {};
+  },
+};
+</script>
